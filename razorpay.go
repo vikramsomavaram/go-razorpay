93d3f443e7144fecdff797829dbbda3ad0d7bf41
Vikram Somavaram <vikram.s@imimobile.com>
2019-05-06 03:39:27 +0530

    add new methods and cleanup

--- razorpay.go
+++ razorpay.go
@@ -262,6 +262,11 @@ type PaymentLinkResponse struct {
 	CreatedAt           int           `json:"created_at"`
 }
 
+type Response struct{
+	Success bool `json:"success"`
+}
+
+//CreatePaymentLink Create a new payment link
 func (r *RazorPay) CreatePaymentLink(paylink PaymentLink) (*PaymentLinkResponse, error) {
 	paylinkresp := new(PaymentLinkResponse) 
 	createreqjson, err := json.Marshal(&paylink)
@@ -273,17 +278,31 @@ func (r *RazorPay) CreatePaymentLink(paylink PaymentLink) (*PaymentLinkResponse,
 	return paylinkresp, err
 }
 
-// func (r *RazorPay) GetPaymentLink(order NewOrder) (*Order, error) {
-// 
-// }
-// 
-// func (r *RazorPay) SendPaymentLink(order NewOrder) (*Order, error) {
-// 
-// }
-// 
-// func (r *RazorPay) CancelPaymentLink(order NewOrder) (*Order, error) {
-// 
-// }
+//GetPaymentLink Get payment link by id
+func (r *RazorPay) GetPaymentLink(id string) (*PaymentLinkResponse, error) {
+	getidresp := new(PaymentLinkResponse)
+	resp, err := r.call("GetPaymentLink", nil, id, nil)
+	json.Unmarshal(resp, getidresp)
+	return getidresp, err
+}
+
+//SendPaymentLink Send/Resend payment link notification to the user 
+func (r *RazorPay) SendPaymentLink(id string, medium string) (bool, error) {
+	sendresp := new(Response)
+	id = id+"/notify_by/"+medium
+	resp, err := r.call("SendPaymentLink", nil, id, nil)
+	json.Unmarshal(resp, sendresp)
+	return sendresp.Success, err
+}
+
+//CancelPaymentLink Cancel given link id 
+func (r *RazorPay) CancelPaymentLink(id string) (string, error) {
+	paylinkresp := new(PaymentLinkResponse) 
+	resp, err := r.call("CancelPaymentLink", nil, id + "/cancel", nil)
+	json.Unmarshal(resp, paylinkresp)
+	return paylinkresp.Status, err
+
+}
 
 type NewOrder struct {
 	Amount         int               `json:"amount"`
@@ -327,7 +346,15 @@ func (r *RazorPay) call(operation string, reqbody []byte, pathparams string, que
 	case "CreatePaymentLink":
 		rmethod = "POST"
 		rurl = APIURL + "invoices"
-
+	case "GetPaymentLink":
+		rmethod = "GET"
+		rurl = APIURL + "invoices/" + pathparams
+	case "SendPaymentLink":
+		rmethod = "POST"
+		rurl = APIURL + "invoices/" + pathparams
+	case "CancelPaymentLink":
+		rmethod = "POST"
+		rurl = APIURL + "invoices/" + pathparams
 	default:
 		err := errors.New("Invalid Method/Operation")
 		return nil, err
